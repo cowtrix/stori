f5ee3d85bda335b8ffe8cc2446a6c466f5baf26b
cowtrix <seandgfinnegan@gmail.com>
2020-12-21 00:02:14 +0000

    Added Google auth. Styling and UI

--- Stori/Controllers/HomeController.cs
+++ Stori/Controllers/HomeController.cs
@@ -117,6 +117,11 @@ namespace Stori.Controllers
 			return View();
 		}
 
+		public IActionResult About()
+		{
+			return View();
+		}
+
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error()
 		{
--- Stori/Startup.cs
+++ Stori/Startup.cs
@@ -44,7 +44,15 @@ namespace Stori
 			services.AddControllersWithViews();
 			services.AddRazorPages();
 			services.AddTransient<IEmailSender, EmailSender>();
-			services.Configure<AuthMessageSenderOptions>(Configuration);	
+			services.Configure<AuthMessageSenderOptions>(Configuration);
+			services.AddAuthentication()
+				 .AddGoogle(options =>
+				 {
+					 IConfigurationSection googleAuthNSection =
+						 Configuration.GetSection("Authentication:Google");
+					 options.ClientId = googleAuthNSection["ClientId"];
+					 options.ClientSecret = googleAuthNSection["ClientSecret"];
+				 });
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
